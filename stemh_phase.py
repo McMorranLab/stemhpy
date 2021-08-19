5c235b855a07028ccbf7e855a5a51485667287d7
Andrew Ducharme <51183584+aducharme@users.noreply.github.com>
2021-08-19 00:23:25 -0700

    Update stemh_phase.py
    Implemented multiple large changes to decrease runtime (~3.5 mins for 128 x 128 to 100 seconds):
    1. Cropping of frames prior to computing 2D Fourier transform so to only transform signal. This did not lead to a huge speed up but did vastly improve the resulting phase image
    2. Changed looping from over rows then over columns to over the raveled form of the SparseArray. This form is 3D, with the first level telling you which scan the frame is, and the second and third levels composed of the dense frame. This removed a nested for loop and eliminated the need for expanding sparse data into dense data in every step. The vast majority of the savings realized in this update.
    3. Moved the amplitude extracting and vacuum kernel setup out of the for loops so that it is done in its own wrapped up command once instead of N x N times

--- stemh_phase.py
+++ stemh_phase.py
@@ -12,7 +12,6 @@ import h5py
 import stempy.io as stio
 from stempy.io import sparse_array
 import stempy.image as stim
-from ncempy.io import dm
 
 # our modules
 # import fft_tools
@@ -22,64 +21,54 @@ dataPath = '/Users/andrewducharme/Documents/Data/ercius_210420/data_scan332_th4.
 savePath = '/Users/andrewducharme/Documents/Sparse Code/'
 vacuumPath = '/Users/andrewducharme/Documents/Data/ercius_210420/Other Files/scan332_vacuum.dm4'
 
-# create vacuum kernel
-# read vacuum scan from dm4 file
-vacuumFile = dm.fileDM(vacuumPath)
-vacuum_data = vacuumFile.getDataset(0)['data']
-vacuum_fft = np.fft.fftshift(np.fft.fft2(vacuum_data))
-kernel = np.conj(vacuum_fft)
-
 # extract specimen information
 sa = sparse_array.SparseArray.from_hdf5(dataPath)
 sa = sa[:, :-1]  # cut off flyback frames
 
-scan_x = sa.shape[0]  # same as sp.attrs['Nx'] in hdf5 file metadata
-scan_y = sa.shape[1]  # same as sp.attrs['Ny'] - 1, since flyback is removed
-frame_x = sa.shape[2]
-frame_y = sa.shape[3]
+scan_row_num = sa.shape[0]  # same as sp.attrs['Ny'] in hdf5 file metadata
+scan_col_num = sa.shape[1]  # same as sp.attrs['Nx'] - 1, since flyback is removed
+frame_row_num = sa.shape[2]
+frame_col_num = sa.shape[3]
 
-data = sa.data.reshape((scan_y,scan_x))  # numpy likes row x column, stempy is column x row
+# figure out how much cropping of the Fourier transform is needed once looping
+crop_amount = ds.select_FT_signal(sa.data, frame_row_num, frame_col_num, 0.1)
+crop_start = crop_amount
+row_crop_end = frame_row_num - crop_amount
+col_crop_end = frame_col_num - crop_amount
 
-empty_frame_counter = 0
+# find amplitude at every scan position
+ampMap = ds.find_amplitude(sa)
 
 length = 75  # standardizes area cropped around peaks for analysis
-ampMap = np.zeros((scan_y, scan_x))
-phaseMap = np.zeros((scan_y, scan_x), dtype=np.complex64)
+map = np.zeros(num_frames, dtype=np.complex64)
 
-for i in range(scan_x):
-    if i // 25 == i / 25:
-        print(i)
-    for j in range(scan_y):
-        frame = data[i, j]
+# create vacuum kernel
+vacuum_kernel = ds.calc_vacuum_kernel(vacuumPath)
+kernel_peak = ds.grab_square_sect(vacuum_kernel, length)
 
-        if frame.size == 0:
-            empty_frame_counter = empty_frame_counter + 1
-            ampMap[i,j] = 0
-            phaseMap[i,j] = 0
-            continue
+for i, frame in enumerate(raveled_sa):
+    if not frame.any():
+        map[i] = 0
+        continue
 
-        diffraction_pattern = stim.calculate_sum_sparse(frame, (frame_x, frame_y))
-        fft = np.fft.fftshift(np.fft.rfft2(diffraction_pattern))
-        abs_fft = np.abs(fft)
+    cropped_frame = frame[crop_start:row_crop_end, crop_start:col_crop_end]
+    fft = np.fft.fftshift(np.fft.rfft2(cropped_frame))
+    abs_fft = abs(fft)
 
-        # find 2 largest magnitude points in the fft (0th and 1st order peaks)
-        peaks = ds.fft_find_peaks(abs_fft, 2)
-        zeroth_order = peaks[0, 1:].astype(int)
-        first_order = peaks[1, 1:].astype(int)
+    # find 2 largest magnitude points in the fft (0th and 1st order peaks)
+    peaks = ds.fft_find_peaks(abs_fft, 2)
+    zeroth_order = peaks[0, 1:].astype(int)
+    first_order = peaks[1, 1:].astype(int)
+    # print(peaks)
 
-        fourier_space_peak = ds.grab_square_sect(fft, length, first_order)
-        kernel_peak = ds.grab_square_sect(kernel, length)
+    fourier_space_peak = ds.grab_square_sect(fft, length, first_order)
 
-        # * is element-wise multiplication
-        t_temp = np.sum(kernel_peak * fourier_space_peak)
+    # * is element-wise multiplication
+    t_temp = np.sum(kernel_peak * fourier_space_peak)
 
-        # in notation, np.abs(np.sum(diffraction_pattern)), but same as just length of frame
-        hx_p = frame.shape[0]
-        # t = hx_p * np.exp(1.0j * np.angle(t_temp))
+    map[i] = np.angle(t_temp)
 
-        phaseMap[i,j] = np.angle(t_temp)
-        ampMap[i,j] = hx_p
-
+phaseMap = map.reshape(scan_row_num, scan_col_num)
         
 fig, ax = plt.subplots(1,2)
 ax[0].imshow(np.abs(phaseMap))
