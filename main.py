bf1a56938b525fe3072466f077eda1dc972e676a
levibrown11 <87206245+levibrown11@users.noreply.github.com>
2021-08-20 16:14:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
 """
 This module is just an example of how to use the fft_tools.py and frame_analysis.py modules
 """
-import h5py
-import stempy.image as stim
-import stempy.io as stio
 from stempy.io.sparse_array import SparseArray
-from fft_tools import *
+from frame_analysis import *
+import timeit
 
 
 def main():
+    # stempy hdf5 sparse object
+    sparse_330 = SparseArray.from_hdf5('data/data_scan332_th4.5_electrons_centered.h5')
+    sa = sparse_330[:, :-1]
 
-    sparse_330 = SparseArray.from_hdf5('data/data_scan330_th4.0_electrons_centered.h5')
-
+    # raw data handling
     data_330 = sparse_330.data
     f_index_330 = frame_index(data_330)
     data_flat_330 = np.concatenate(data_330).astype('u4')
 
-    frames_330 = elec_count_frame(data_flat_330, f_index_330, (0, 10000))
-    fft_frames_330 = gen_fft(frames_330)
+    # construct composite image and fft from raw data
+    full_330 = elec_count_full(data_flat_330)
+    fft_full_330 = gen_fft(full_330)
+    full_peaks = fft_find_peaks(np.abs(fft_full_330), 10)
 
-    peaks = fft_find_peaks(np.abs(fft_frames_330),5)
-    mask_330 = fft_mask(fft_frames_330, peaks[2], 70, 'square')
+    # crop out vacuum space
+    crop = crop_vacuum(full_330, 0.2)
+    cropped_full = full_330[crop:full_330.shape[0] - crop,
+                   crop:full_330.shape[0] - crop]
 
-    plots(frames_330, np.abs(fft_frames_330))
-    plots(frames_330 * mask_330, np.abs(fft_frames_330) * mask_330)
+    # index of first order fourier peak
+    first_peak = np.array([full_peaks[1, 1], full_peaks[1, 2]])
+
+    fft_peaks = []
+    start = timeit.default_timer()
+    # main loop, computes an fft and stores 1st order peak of each frame
+    for i in np.arange(len(f_index_330)):
+        frame = analyze_frame(data_flat_330, f_index_330, i, crop, first_peak)  # order 0.003
+        fft_peaks.append(frame)
+    end = timeit.default_timer()
+    time = end-start
+    # # takes around 117 seconds
+    # raveled_sa = sa.ravel_scans()
+    # for i, frame in enumerate(raveled_sa):
+    #     cropped_frame = frame[crop:frame.shape[0] - crop,
+    #                     crop:frame.shape[0] - crop]
+    #     fft = gen_fft(cropped_frame)
+    #     fft_peaks.append(fft[first_peak[0], first_peak[1]])
+
+    plots(cropped_full, np.abs(fft_full_crop))
 
     plt.show()
 
 
 if __name__ == "__main__":
     main()
+
