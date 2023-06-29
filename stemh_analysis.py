6bb44fc82c385a3032d4c52fdf76234f2a055e97
Andrew Ducharme <51183584+aducharme@users.noreply.github.com>
2023-06-29 13:17:08 -0700

    Update stemh_analysis.py
    Code is now able to do I4DSTEM analysis by grabbing the value of a user-chosen Fourier peak. Usage of a vacuum kernel is not set up, but this is made more clear in the comments

--- stemh_analysis.py
+++ stemh_analysis.py
@@ -1,9 +1,14 @@
 ## accelerated version of analysis method used in Yasin et al. APL 133, 23102 (2018)
 ## and Harvey et al. Phys. Rev. Applied 10, 061001 (2018).
-## this uses a reference frame inside the analysis itself, which introduces further error
-## and assumes the value the FFT produces as the phase is accurate, which is not true
-## due to leakage and other discrete artifacts.
 
+## This process would optimally use the 0th order Fourier peak of a frame
+## where all probes go through vacuum. this can remove geometric phase. 
+## the part of this script that would set this up does not compile
+## however, the script as a whole should still work
+
+## This process assumes the value the FFT produces as the phase is accurate,
+## which is not true due to the analogue of leakage for phase
+ 
 
 # base python modules
 import numpy as np
@@ -36,10 +41,9 @@ ampMap = st.calc_amplitude(sa)
 ravelled_sa = sa.ravel_scans()  # take sparse 4Dstem data and flatten into 3D dense
 num_frames = ravelled_sa.shape[0]
 
-# this section computes 3 quantities we'll be using in every step of the loop
-# Do so with a representative frame in the dataset
-# finds 1. location of the Fourier peaks, 2. how large a square will be selected around first order peak,
-# 3. angle of diffraction probes in real space
+# this section computes 2 quantities we'll be using in every step of the loop
+# do so with a representative frame in the dataset
+# finds 1. location of the Fourier peaks and 2. how large a square will be selected around first order peak.
 
 rep_frame = ravelled_sa[num_frames // 2]  # get representative frame from center of measurement
 rep_fft = st.fftw2D(rep_frame)
@@ -48,14 +52,13 @@ fft_peaks = st.fft_find_peaks(rep_fft, 2)  # find two highest magnitude peaks in
 
 first_order = fft_peaks[1, 1:]  # location of first order peak
 selection_size = st.calc_box_size(fft_peaks)
-probe_angle = st.calc_inplane_angle(fft_peaks)
 
 # initialize array to store values through loop
 phaseMap = np.zeros(num_frames, dtype=np.complex64)
 
-# create vacuum kernel
-vacuum_kernel = st.kernelize_vacuum_scan(ravelled_sa[50*50])  # human input needed to select what is truly a vacuum frame
-kernel_peak = st.grab_square_box(vacuum_kernel, selection_size)  # functionally a Dirac delta
+# # create vacuum kernel
+# vacuum_kernel = st.kernelize_vacuum_scan(ravelled_sa[50*50])  # human input needed to select what is truly a vacuum frame
+# kernel_peak = st.grab_square_box(vacuum_kernel, selection_size)  # functionally a Dirac delta
 
 # setting up pyfftw numpy interface
 pyfftw.config.NUM_THREADS = multiprocessing.cpu_count()
@@ -78,12 +81,11 @@ for i, frame in enumerate(ravelled_sa):
     ft = pyfftw.interfaces.numpy_fft.rfft2(input)  # take Fourier transform of the full frame
     ft = pyfftw.interfaces.numpy_fft.fftshift(ft)
 
-    fourier_space_peak = st.grab_square_box(ft, selection_size, first_order)  # select the area around the first peak
+    # fourier_space_peak = st.grab_square_box(ft, selection_size, first_order)  # select the area around the first peak
+    # phaseMap[i] = np.sum(kernel_peak * fourier_space_peak)  # Integrate kernel and first order peak product
+    phaseMap[i] = ft[first_order[0], first_order[1]]
 
-    # phase computation 
-    t_temp = np.sum(kernel_peak * fourier_space_peak)  # convolve kernel and first order peak (* multiplies elementwise)
-    phaseMap[i] = np.angle(t_temp)  
-
+phaseMap = np.angle(phaseMap)  # phase computation 
 phaseMap = phaseMap.reshape(scan_row_num, scan_col_num)
 
 end = time.time()
@@ -93,7 +95,7 @@ print(str((end - start) / scan_row_num / scan_col_num))
 print('1024 x 1024 time (min): ' + str((end - start) / scan_row_num / scan_col_num * 1024 * 1024 / 60))
 
 fig, ax = plt.subplots(1,2)
-ax[0].imshow(np.abs(phaseMap))
+ax[0].imshow(phaseMap)
 ax[1].imshow(ampMap)
 plt.show()
 plt.close('all')
